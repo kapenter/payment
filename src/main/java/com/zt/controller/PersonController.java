eb456060f117660faf6f9225eba8612e39b333c7
zhouwei <873207695@qq.com>
2018-05-24 11:20:17 +0800

    提交的文件

---
+++ src/main/java/com/zt/controller/PersonController.java
@@ -0,0 +1,22 @@
+package com.zt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: payment
+ * @description:
+ * @author: zhou.wei
+ * @create: 2018-05-24 11:02
+ **/
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @RequestMapping("referPerson")
+    public String  referPerson(){
+        System.out.println("i  am  jack ");
+        return "success";
+    }
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xn086532
+  Date: 2018/5/24 0024
+  Time: 10:53
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>支付</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
